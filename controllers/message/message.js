a121bfae150914346756803d93c1f54bfbed3b70
rumor1993 <rumor1993@rumor1993ui-MacBookAir.local>
2021-05-03 23:43:31 +0900

    return false 추가

--- controllers/message/message.js
+++ controllers/message/message.js
@@ -24,12 +24,12 @@ exports.createMessage = async ( req , res ) => {
 
     if (!req.body.sender) {
         res.send("보낸사람이 없습니다")
-        res.end()
+        return false
     } 
 
     if (!req.body.sex) {
         res.send("성별이 없습니다")
-        res.end()
+        return false
     }
     
     if (isRandom) {
