57717424269aad4b5d465cf7112c77d1a234082e
rumor1993 <rumor1993@rumor1993ui-MacBookAir.local>
2021-05-25 00:49:40 +0900

    API Document

--- apiDoc/apidoc/api_project.js
+++ apiDoc/apidoc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.1.0",
   "description": "apiDoc basic example",
   "title": "Custom apiDoc browser title",
-  "url": "https://api.github.com/v1",
+  "url": "http://13.209.40.23:3000",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-24T15:48:37.982Z",
+    "time": "2021-05-24T15:49:37.223Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
