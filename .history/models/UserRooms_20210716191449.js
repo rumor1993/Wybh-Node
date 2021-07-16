fa046011c2696241889007b983ce67f67ce94f35
rumor1993 <rumor1993@naver.com>
2021-07-16 19:15:06 +0900

    rumor1993

--- .history/models/UserRooms_20210716191449.js
+++ .history/models/UserRooms_20210716191449.js
@@ -4,14 +4,25 @@ const models = require("../models");
 module.exports = function (sequelize, DataTypes) {
   const UserRooms = sequelize.define("UserRooms", {
     user_id: { type: DataTypes.STRING },
-    room_id: { type: DataTypes.STRING },
+    room_id: { type: DataTypes.INTEGER, primaryKey: true },
     last_message: { type: DataTypes.STRING },
     profile_image: { type: DataTypes.STRING },
     room_user_name: { type: DataTypes.STRING },
     room_user_list: { type: DataTypes.STRING },
+    room_user_sex: { type: DataTypes.STRING },
   });
 
-  UserRooms.associate = function () {};
+  UserRooms.associate = function (models) {
+    models.UserRooms.hasMany(models.Message, {
+      foreignKey: "room_id",
+      sourceKey: "room_id",
+    });
+
+    models.UserRooms.belongsTo(models.Users, {
+      foreignKey: "room_user_list",
+      sourceKey: "id",
+    });
+  };
 
   return UserRooms;
 };
